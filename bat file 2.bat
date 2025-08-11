b399ddb1ee1206c5a7fdf961fa8b6c0ccfcf20e7
Marcus Ureta <silvergreat0@gmail.com>
2025-08-11 18:21:28 +0800

    64 bit change

--- bat file 2.bat
+++ bat file 2.bat
@@ -1,8 +1,8 @@
 @echo on
 
-g++ -m32 -Wall -O2 -std=c++17 -c pt1.cpp
-g++ -m32 -Wall -O2 -std=c++17 -c pt1_1.cpp
-g++ -m32 -Wall -O2 -std=c++17 pt1_1.o pt1.o -o program
+g++ -m64 -Wall -O2 -std=c++17 -c pt1.cpp
+g++ -m64 -Wall -O2 -std=c++17 -c pt1_1.cpp
+g++ -m64 -Wall -O2 -std=c++17 pt1_1.o pt1.o -o program
 
 pause
 exit
